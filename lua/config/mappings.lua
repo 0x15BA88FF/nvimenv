7d0a664214dfaa3c33aadb6a9aceb1fb9488d433
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-01-02 09:40:58 +0000

    Change: update mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,4 +1,32 @@
 local map = vim.keymap.set
+local lazymap = LazyVim.safe_keymap_set
+
+lazymap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+lazymap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+lazymap({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+lazymap({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+
+lazymap("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+lazymap("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+lazymap("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+lazymap("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+lazymap("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+lazymap("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+lazymap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+lazymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+lazymap("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+lazymap("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+lazymap("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+lazymap("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+lazymap("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+lazymap("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+lazymap("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+lazymap("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+
+-- [[ Custom Mappings ]]
 
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
@@ -6,7 +34,7 @@ map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 map("i", "jk", "<ESC>", { desc = "Quick escape" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
-map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
