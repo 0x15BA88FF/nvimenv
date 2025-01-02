63fd9b393ec97f6026a598ddbdb5f1ab82724a05
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-01-02 10:03:45 +0000

    Fixed: add augroup function

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,7 @@
+local function augroup(name)
+    return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = augroup("highlight_yank"),
