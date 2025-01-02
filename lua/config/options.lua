69efe93d99b7f30dad962d8baa09cd093805fceb
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-01-02 08:50:19 +0000

    Change: update nvim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,54 +1,90 @@
 local g = vim.g
 local opt = vim.opt
 
-g.have_nerd_font = false
+g.autoformat = true
+g.have_nerd_font = true
+g.snacks_animate = false
+g.trouble_lualine = true
 g.dap_virtual_text = true
+g.lazyvim_cmp = "auto"
+g.lazyvim_picker = "auto"
+g.deprecation_warnings = true
 
 opt.mouse = "a"
+opt.cursorline = true
+opt.cursorlineopt = "both"
+opt.number = true
+opt.relativenumber = true
+opt.ruler = true
+opt.virtualedit = "block"
+
 opt.showmode = false
 
 opt.hlsearch = true
 opt.incsearch = true
 opt.smartcase = true
 opt.ignorecase = true
-
-opt.number = true
-opt.cursorline = true
-opt.relativenumber = true
-opt.cursorlineopt = "both"
+opt.inccommand = "nosplit"
+opt.wildmode = "longest:full,full"
 
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
 opt.expandtab = true
+opt.shiftround = true
 opt.smartindent = true
 opt.breakindent = true
 
 opt.wrap = false
 opt.colorcolumn = "80"
+opt.linebreak = true
+opt.conceallevel = 2
+opt.list = true
+opt.listchars = {
+    trail = "·",
+    tab = "» ",
+    nbsp = "␣"
+}
+opt.fillchars = {
+    foldclose = "",
+    foldopen = "",
+    foldsep = " ",
+    fold = " ",
+    diff = "╱",
+    eob = " "
+}
+
+opt.splitright = true
+opt.splitbelow = true
+opt.splitkeep = "screen"
+opt.scrolloff = 10
+opt.signcolumn = "yes"
+opt.laststatus = 3
+opt.winminwidth = 5
+
+opt.completeopt = "menu,menuone,noselect"
+opt.pumheight = 10
+opt.pumblend = 10
+
+opt.backup = false
+opt.swapfile = false
+opt.undofile = true
+opt.undolevels = 10000
+opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
 
 opt.updatetime = 250
 opt.timeoutlen = 300
 
-opt.scrolloff = 10
-opt.signcolumn = "yes"
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.shortmess:append({ W = true, I = true, c = true, C = true })
+
+opt.foldlevel = 99
 opt.termguicolors = true
 opt.isfname:append("@-@")
 
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
-opt.conceallevel = 2
-opt.completeopt = "menuone,noselect"
-
-opt.backup = false
-opt.undofile = true
-opt.swapfile = false
-opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+opt.confirm = true
+opt.autowrite = true
 
 vim.schedule(function()
-    opt.clipboard = "unnamedplus"
+    opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
 end)
